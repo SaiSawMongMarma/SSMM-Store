8b5edb81f69a66ea617dd67605f50b59ad23a816
sai saw mong <saisawmong0@gmail.com>
2026-09-23 19:36:42 +0600

    ok

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Destructuring & coming from env file
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD} = process.env;
+
+// create a sql connection using our env variables
+export const sql = neon(
+    `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`
+)
+
+// this sql function we export is used as a tagged template literal, which allows us to write sql queries safely
